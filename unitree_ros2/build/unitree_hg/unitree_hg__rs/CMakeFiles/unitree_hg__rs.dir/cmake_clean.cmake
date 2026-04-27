2f256e60581ea62f8f483f44376f99ea4513cf12
hyc <hyc021118@gmail.com>
2026-04-27 05:50:12 +0000

    将子模块转换为普通文件夹

---
+++ unitree_ros2/build/unitree_hg/unitree_hg__rs/CMakeFiles/unitree_hg__rs.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "/home/unitree/go2slam/unitree_ros2/build/unitree_hg/rosidl_generator_rs/unitree_hg/rust/Cargo.toml"
+  "/home/unitree/go2slam/unitree_ros2/build/unitree_hg/rosidl_generator_rs/unitree_hg/rust/build.rs"
+  "/home/unitree/go2slam/unitree_ros2/build/unitree_hg/rosidl_generator_rs/unitree_hg/rust/src/lib.rs"
+  "/home/unitree/go2slam/unitree_ros2/build/unitree_hg/rosidl_generator_rs/unitree_hg/rust/src/msg.rs"
+  "/home/unitree/go2slam/unitree_ros2/build/unitree_hg/rosidl_generator_rs/unitree_hg/rust/src/msg/rmw.rs"
+  "CMakeFiles/unitree_hg__rs"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/unitree_hg__rs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
